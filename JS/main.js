b9295a1d2a8bb775e24ff530b288f565dae6a2c7
Vitalii Dubinin <98089841+VitaliiDubinin@users.noreply.github.com>
2022-07-13 13:57:32 +0300

    animated string added

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,16 @@
 jQuery(document).ready(function () {
   "use strict";
-  $(".div1,.div2,.div3,.div4").ripples({
-    dropRadius: 15,
-    perturbance: 0.01,
-  });
+  // $(".div1,.div2,.div3,.div4").ripples({
+  //   dropRadius: 15,
+  //   perturbance: 0.01,
+  // });
+  // $(".text").typed({
+  //   strings: ["Fist sentence.", "Second sentence."],
+  //   typeSpeed: 50,
+  //   backSpeed: 50,
+  //   loop: true,
+  // });
 });
+
+let typed = new Typed(".welText", { strings: ["test string1", "test string2"], typeSpeed: 50, backSpeed: 10, loop: true });
+// let typed = new Typed(".text", { strings: ["test string1", "test string2"], typeSpeed: 50, backSpeed: 50, loop: true });
