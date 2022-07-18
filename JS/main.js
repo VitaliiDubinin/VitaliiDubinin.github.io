8a391903f1bdb9072e1a5388de1a99e181c12954
Vitalii Dubinin <98089841+VitaliiDubinin@users.noreply.github.com>
2022-07-18 13:48:26 +0300

    animated string works

--- JS/main.js
+++ JS/main.js
@@ -9,8 +9,26 @@ jQuery(document).ready(function () {
   //   typeSpeed: 50,
   //   backSpeed: 50,
   //   loop: true,
+  //   showCursor: false,
   // });
 });
 
-let typed = new Typed(".welText", { strings: ["test string1", "test string2"], typeSpeed: 50, backSpeed: 10, loop: true });
+// $(function () {
+//   $(".text").Typed({
+//     strings: ["First sentence.", "Second sentence."],
+//     typeSpeed: 10,
+//   });
+// });
+
+let typed = new Typed(".welText", {
+  strings: [
+    "<strong> welcome to my </strong> <strong class=primary> portfolio!!!</strong>",
+    "<strong>Nice to </strong><strong class=primary> meet YOU!!!</strong>",
+  ],
+  typeSpeed: 50,
+  backSpeed: 10,
+  loop: true,
+  // showCursor: false,
+});
+// Typed.new(".welText", { strings: ["test string1", "test string2"], contentType: "html" });
 // let typed = new Typed(".text", { strings: ["test string1", "test string2"], typeSpeed: 50, backSpeed: 50, loop: true });
